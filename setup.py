f36e6df0fd1901b494b798e2f9100b3018c8446e
mcalla314 <mcalla314@gmail.com>
2019-02-19 15:58:45 -0500

    fix package list

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mcalla314/tkintertoy/archive/v1.0.0.tar.gz",
-    packages=[tkintertoy],
+    packages=['tkintertoy'],
     keywords=['GUI','Tkinter'],
     classifiers=[
         "Programming Language :: Python :: 2.7",

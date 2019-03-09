bee3ba9d42ee89bd2e56381846b78d06ec132ece
mcalla314 <mcalla314@gmail.com>
2019-03-09 09:32:18 -0500

    added highlight_language to turn on syntax highlighting

--- docs/conf.py
+++ docs/conf.py
@@ -70,6 +70,9 @@ release = u'1.0.0'
 # Usually you set "language" from the command line for these cases.
 language = None
 
+# added to turn on syntax highlighting
+highlight_language='python3'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
@@ -87,8 +90,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'bizstyle' 
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
